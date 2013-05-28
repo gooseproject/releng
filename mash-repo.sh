17f5f4932708d4ba4df70b0afab7abb522d7639d
Clint Savage <herlo@gooseproject.org>
2013-05-27 20:22:29 -0600

    fixup mash-repo.sh for updates

--- mash-repo.sh
+++ mash-repo.sh
@@ -11,21 +11,32 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+#./mash-repo.sh 6.0 Updates Final gold
+
 VERSION=$1
 STAGE=$2
 SEQ=$3
 CFG=$4
 
-BASE_DIR=/mnt/koji/releng/gl6
-if [ "${STAGE}" == "Alpha" ]; then
+MAJOR=$(echo ${VERSION} | cut -f1 -d".")
+
+STG_LOWER=$(echo ${STAGE} | tr '[A-Z]' '[a-z]')
+SEQ_LOWER=$(echo ${SEQ} | tr '[A-Z]' '[a-z]')
+
+BASE_DIR=/mnt/koji/releng/gl${MAJOR}
+if [ "${STG_LOWER}" == "updates" ]; then
+    if [ "${SEQ_LOWER}" != "final" ]; then
+        OUTPUT_DIR=${BASE_DIR}/testing/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
+    else
+        OUTPUT_DIR=${BASE_DIR}/testing/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
+    fi
+elif [ "${STG_LOWER}" == "alpha" ]; then
     REL_DIR="sketchy"
-    OUTPUT_DIR=${BASE_DIR}/${REL_DIR}/${VERSION}-${STAGE}-${SEQ}/
-elif [ "${STAGE}" == "Beta" ]; then
-    REL_DIR="${VERSION}"
-    OUTPUT_DIR=${BASE_DIR}/${REL_DIR}/${STAGE}-${SEQ}/
-else
-    REL_DIR="${VERSION}"
-    OUTPUT_DIR=${BASE_DIR}/${REL_DIR}/
+    OUTPUT_DIR=${BASE_DIR}/${REL_DIR}/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
+elif [ "${STG_LOWER}" == "beta" ] || [ "${SEQ_LOWER}" != "final" ]; then
+    OUTPUT_DIR=${BASE_DIR}/testing/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
+elif [ "${STG_LOWER}" == "final" ] || [ "${SEQ_LOWER}" == "final" ]; then
+    OUTPUT_DIR=${BASE_DIR}/releases/${VERSION}/
 fi
 
 if [ ! -d ${OUTPUT_DIR} ]; then
@@ -33,13 +44,25 @@ if [ ! -d ${OUTPUT_DIR} ]; then
     mkdir -p ${OUTPUT_DIR};
 fi
 
-mash -o ${OUTPUT_DIR} ${CFG} -f /mnt/koji/releng/comps-goose6-server.xml
+if [ "${STG_LOWER}" == "updates" ]; then
+    mash -o ${OUTPUT_DIR} ${CFG}
+else
+    mash -o ${OUTPUT_DIR} ${CFG} -f /mnt/koji/releng/comps-goose6-server.xml
+fi
 
 pushd ${OUTPUT_DIR} # &> /dev/null
-rsync -a ${CFG}/* Everything/
-rm -rf ${CFG}
+
+if [ "${STG_LOWER}" == "updates" ]; then
+    rsync -a ${CFG}/* .
+    rm -rf ${CFG}
+else
+    rsync -a ${CFG}/* Everything/
+    rm -rf ${CFG}
+fi
+
 popd # &> /dev/null
 
-rm -f ${BASE_DIR}/6{,Server}
-ln -s ${OUTPUT_DIR} /mnt/koji/releng/gl6/6
-ln -s ${OUTPUT_DIR} /mnt/koji/releng/gl6/6Server
+if [ "${STG_LOWER}" == "final" ] || [ "${SEQ_LOWER}" == "final" ]; then
+    rm -f ${BASE_DIR}/${MAJOR}
+    ln -s ${OUTPUT_DIR} /mnt/koji/releng/gl${MAJOR}/${MAJOR}
+fi
