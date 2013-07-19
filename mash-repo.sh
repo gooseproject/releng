2d509331467e4812d39936806814618de5dfe6d7
Clint Savage <herlo@gooseproject.org>
2013-07-18 20:58:46 -0600

    updating mash-repo to use the standard directory structure now

--- mash-repo.sh
+++ mash-repo.sh
@@ -20,38 +20,19 @@ CFG=$4
 
 MAJOR=$(echo ${VERSION} | cut -f1 -d".")
 
-<<<<<<< HEAD
 STG_LOWER=$(echo ${STAGE} | tr '[A-Z]' '[a-z]')
 SEQ_LOWER=$(echo ${SEQ} | tr '[A-Z]' '[a-z]')
 
 BASE_DIR=/mnt/koji/releng/gl${MAJOR}
 if [ "${STG_LOWER}" == "updates" ]; then
     if [ "${SEQ_LOWER}" != "final" ]; then
-        OUTPUT_DIR=${BASE_DIR}/testing/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
+        CFG="updates-candidate"
     else
-        OUTPUT_DIR=${BASE_DIR}/testing/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
+        CFG="updates"
     fi
-elif [ "${STG_LOWER}" == "alpha" ]; then
-    REL_DIR="sketchy"
-    OUTPUT_DIR=${BASE_DIR}/${REL_DIR}/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
-elif [ "${STG_LOWER}" == "beta" ] || [ "${SEQ_LOWER}" != "final" ]; then
-    OUTPUT_DIR=${BASE_DIR}/testing/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
-elif [ "${STG_LOWER}" == "final" ] || [ "${SEQ_LOWER}" == "final" ]; then
-    OUTPUT_DIR=${BASE_DIR}/releases/${VERSION}/
-=======
-
-BASE_DIR=/mnt/koji/releng/gl${MAJOR}
-if [ "${STAGE}" == "Alpha" ]; then
-    REL_DIR="sketchy"
-    OUTPUT_DIR=${BASE_DIR}/${REL_DIR}/${VERSION}-${STAGE}-${SEQ}/
-elif [ "${STAGE}" == "Beta" ] || [ "${SEQ}" != "Final" ]; then
-    REL_DIR="${VERSION}"
-    OUTPUT_DIR=${BASE_DIR}/${REL_DIR}/${STAGE}-${SEQ}/
-else
-    REL_DIR="${VERSION}"
-    OUTPUT_DIR=${BASE_DIR}/${REL_DIR}/Final/
->>>>>>> 574c2021609816b3533c87b5bc08fbd51ea80146
 fi
+
+OUTPUT_DIR=${BASE_DIR}/testing/${VERSION}/${STG_LOWER}-${SEQ_LOWER}/
 
 if [ ! -d ${OUTPUT_DIR} ]; then
     echo "$(date +'%Y-%m-%d %H:%M:%S') $0: creating ${OUTPUT_DIR}"
@@ -64,7 +45,7 @@ else
     mash -o ${OUTPUT_DIR} ${CFG} -f /mnt/koji/releng/comps-goose6-server.xml
 fi
 
-pushd ${OUTPUT_DIR} # &> /dev/null
+pushd ${OUTPUT_DIR} &> /dev/null
 
 if [ "${STG_LOWER}" == "updates" ]; then
     rsync -a ${CFG}/* .
@@ -74,14 +55,4 @@ else
     rm -rf ${CFG}
 fi
 
-popd # &> /dev/null
-
-<<<<<<< HEAD
-if [ "${STG_LOWER}" == "final" ] || [ "${SEQ_LOWER}" == "final" ]; then
-    rm -f ${BASE_DIR}/${MAJOR}
-    ln -s ${OUTPUT_DIR} /mnt/koji/releng/gl${MAJOR}/${MAJOR}
-fi
-=======
-rm -f ${BASE_DIR}/${MAJOR}
-ln -s ${OUTPUT_DIR} /mnt/koji/releng/gl${MAJOR}/${MAJOR}
->>>>>>> 574c2021609816b3533c87b5bc08fbd51ea80146
+popd &> /dev/null
